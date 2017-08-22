2318a0c6d54664edc198c3c31efab7f14d29c184
jeikerxiao <jeiker@126.com>
2017-08-22 10:56:15 +0800

    init spring-cloud-config project
    init spring-cloud-config project

---
+++ spring-cloud-config/config-client/src/main/java/com/jeiker/demo/web/HelloController.java
@@ -0,0 +1,29 @@
+package com.jeiker.demo.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author : xiao
+ * @Date : 17/8/22 上午10:10
+ */
+@RestController
+public class HelloController {
+
+    @Value("${message}")
+    String message;
+
+    @GetMapping("/hello")
+    public Map<String, String> hello(@RequestParam String name) {
+        Map<String, String> result = new HashMap<>();
+        result.put("name", name);
+        result.put("message", message);
+        return result;
+    }
+
+}
