d842413238a5c21433104aaeba2bcf0dd47fd659
jeikerxiao <jeiker@126.com>
2017-09-18 09:02:54 +0800

    init spring-cloud-eureka3
    init spring-cloud-eureka3

---
+++ spring-cloud-eureka3/src/main/java/com/jeiker/demo/SpringCloudEureka3Application.java
@@ -0,0 +1,12 @@
+package com.jeiker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEureka3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEureka3Application.class, args);
+	}
+}
